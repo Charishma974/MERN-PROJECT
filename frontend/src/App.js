91072e969ad5c9df7f5ce14bd4faae5b890b7f5e
Charishma974 <cherryspl09@gmail.com>
2024-06-06 23:22:10 +0530

    Created Frontend with Header,Basic Home,Footer component Successfully.

---
+++ frontend/src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css"
+import WebFont from "webfontloader";
+import { useEffect } from "react";
+import Header from "./component/layout/Header/Header.js";
+import Footer from "./component/layout/Footer/Footer.js";
+import Home from "./component/Home/Home.js";
+
+function App() {
+
+  useEffect(() => {
+
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"]
+      }
+    })
+  }, [])
+  return <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+    </Routes>
+    <Footer />
+  </BrowserRouter>
+}
+
+export default App;
