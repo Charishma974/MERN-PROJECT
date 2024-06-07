447bab4a6176a6962aad24a27a30c75c19550cf3
Charishma974 <cherryspl09@gmail.com>
2024-06-07 08:06:56 +0530

    Created Products in Home page Successfully.

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import {CgMouse} from "react-icons/cg";
 import "./Home.css";
+import Product from "./Product.js";
+import MetaData from '../layout/MetaData.js';
+
+const product={
+  name: "Blue T-shirt",
+  images: [{url: "https://i.ibb.co/DRST11n/1.webp"}],
+  price: "₹3000",
+  _id: "abhishek",
+}
 
 const Home = () => {
   return (
     <>
+
+    <MetaData title="ECOMMERCE" />
+
         <div className="banner">
             <p>Welcome to Ecommerce</p>
             <h1>FIND AMAZING PRODUCTS BELOW</h1>
@@ -17,6 +29,19 @@ const Home = () => {
         </div>
 
         <h2 className="homeHeading">Featured Products</h2>
+
+        <div className="container" id="container">
+          <Product product={product} />
+          <Product product={product} />
+          <Product product={product} />
+          <Product product={product} />
+
+          <Product product={product} />
+          <Product product={product} />
+          <Product product={product} />
+          <Product product={product} />
+
+        </div>
     </>
   )
 }
